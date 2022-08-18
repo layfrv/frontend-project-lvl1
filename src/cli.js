c7154882240c61a0e585bf03eee8c880684df794
layfr <laylafirsova@gmail.com>
2022-08-18 14:48:56 +0700

    fix

--- src/cli.js
+++
@@ -1,7 +0,0 @@
-import readlineSync from 'readline-sync';
-
-export const userName = () => {
-    const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
-};
-
