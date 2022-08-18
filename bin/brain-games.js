1b19beef58cf549bc808e5f235539697c79adf41
layfr <laylafirsova@gmail.com>
2022-08-18 19:23:07 +0700

    fix

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,7 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!')
+// eslint-disable-next-line import/extensions, import/named
+import { userName } from '../src/cli.js';
+
+// eslint-disable-next-line no-console
+console.log('Welcome to the Brain Games!');
+userName();
